4abb55313fe3fa537e11ec040dd81021e0342d87
mupetmower <37377303+mupetmower@users.noreply.github.com>
2018-04-12 14:42:40 -0400

    Product Image uploading

--- src/main/java/com/houseofcards/controllers/ProductController.java
+++ src/main/java/com/houseofcards/controllers/ProductController.java
@@ -5,12 +5,19 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.houseofcards.entities.generated.Products;
 import com.houseofcards.repositories.ProductImageRepository;
 import com.houseofcards.services.ProductService;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.ClassPathResource;
 
 @Controller
 public class ProductController {
@@ -33,14 +40,46 @@ public class ProductController {
     
     @RequestMapping("product/new")
     public String newProduct(Model model){
-	model.addAttribute("product", new Products());
-	return "productform";
+    	model.addAttribute("product", new Products());
+    	return "productform";
     }
     
     @RequestMapping(value = "product", method = RequestMethod.POST)
-    public String saveProduct(Products product){
-	productService.save(product);
-	return "redirect:/product/" + product.getPkProductId();
+    public String saveProduct(Products product, MultipartFile thumbnailFileChooser, String thumbnailUri){
+    	
+    	
+    	try {
+    		File staticRes = new ClassPathResource("static").getFile();
+    		
+    		String newPath2 = staticRes.getAbsolutePath() + thumbnailUri;
+    		
+    		
+    		String newPath = thumbnailUri;
+    		
+    		product.setThumbnailUri(thumbnailUri);
+    		
+    		File newImg = new File(newPath2);
+    		
+    		
+    		thumbnailFileChooser.transferTo(newImg);
+    		
+    					
+		} catch (IllegalStateException e) {
+			System.out.println(e.getMessage());
+			e.printStackTrace();
+		} catch (IOException e) {
+			System.out.println(e.getMessage());
+			e.printStackTrace();
+		}
+    	
+
+    	
+    	productService.save(product);
+    	
+    	
+    	
+    	
+    	return "redirect:/product/" + product.getPkProductId();
     }
     
     @RequestMapping("product/{id}")
@@ -57,15 +96,16 @@ public class ProductController {
     
     @RequestMapping("product/edit/{id}")
     public String edit(@PathVariable Integer id, Model model){
-	model.addAttribute("product", productService.getById(id));
-	return "productform";
+    	model.addAttribute("product", productService.getById(id));
+    	model.addAttribute("productImages", productService.getById(id).getProductimages());
+    	return "productform";
     }
     
     
     @RequestMapping("product/delete/{id}")
     public String delete(@PathVariable Integer id){
-	productService.delete(id);
-	return "redirect:/products";
+    	productService.delete(id);
+    	return "redirect:/products";
     }
     
     
