56cbc360970e5c14cb43febf69c86200e2713480
mupetmower <37377303+mupetmower@users.noreply.github.com>
2018-05-02 11:09:57 -0400

    Top Products

--- src/main/java/com/houseofcards/controllers/TopProductsController.java
+++ src/main/java/com/houseofcards/controllers/TopProductsController.java
@@ -1,13 +1,56 @@
 package com.houseofcards.controllers;
 
-import com.houseofcards.entities.generated.Topproducts;
-import com.houseofcards.repositories.EntityRepository;
-import com.houseofcards.services.EntityService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
+import com.houseofcards.entities.generated.Topproducts;
+import com.houseofcards.repositories.TopProductsRepository;
+
+
+@Controller
+@RequestMapping("/admin/topproducts")
 public class TopProductsController {
 	
+	@Autowired
+	private TopProductsRepository topProductRepo;
+	
+	
+	@RequestMapping(value = "/", method = RequestMethod.GET)
+	public String listAllTopProducts(Model model) {
+		model.addAttribute("topproducts", topProductRepo.findAll());
+		
+		return "topproducts";
+	}
+	
+	
+	@RequestMapping(value = "/", method = RequestMethod.POST)
+	public String listAllTopProducts(Topproducts topProduct, Model model) {
+		topProductRepo.save(topProduct);
+		
+		model.addAttribute("topproducts", topProductRepo.findAll());
+		
+		return "topproducts";
+	}
+	
+	
+	@RequestMapping("/new")
+	public String addTopProduct(Model model, @PathVariable Integer topProductId) {
+		model.addAttribute("topproduct", new Topproducts());
+		
+		return "topproductform";
+	}
 	
 	
 	
+	@RequestMapping("/remove/{topProductId}")
+	public String removeTopProduct(Model model, @PathVariable Integer topProductId) {
+		topProductRepo.delete(topProductRepo.findById(topProductId).get());
+		
+		return "topproducts";
+	}
 	
 }
